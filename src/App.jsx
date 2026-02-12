7dd0dd064d7c300d0948677241149809267c52b3
Aravind <gmarav005@gmail.com>
2026-02-12 21:23:30 +0530

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import BasicProps from './components/BasicProps'
+import RefProps from './components/RefProps'
+import ChildrenProps from './components/ChildrenProps'
+import ComplexProps from './components/ComplexProps'
+import ThemeToggler from './components/ThemeToggler'
+
+function Navigation() {
+  const isDark = true;
+  const section = [
+    {id: 'basic', label: 'Basic Props', icon: '📦'}
+    {id: 'ref', label: 'Ref Props', icon: '🔗'}
+    {id: 'children', label: 'Children Props', icon: '👶'}
+    {id: 'complex', label: 'Complex Props', icon: '🧩'}
+    {id: 'theme', label: 'Theme Props', icon: '🎨'}
+  ]
+
+  return (
+    <nav className={`sticky top-0 z-50 shadow-md`}></nav>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
